b59a0e79060d9283cbfebae4224c660eeedb5674
Joe P Shoulak <joe@squarecompass.io>
2024-03-19 12:14:36 -0500

    polish

--- grapher.ino
+++ grapher.ino
@@ -1,8 +1,7 @@
 #include <Wire.h>
 #include <U8g2lib.h>
 #include "CircularBuffer.h"
-#include <float.h>
-#include "Graph.h"
+// #include "Graph.h"
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -15,22 +14,6 @@ CircularBuffer<float, POINT_COUNT> dataPoints;
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
 
 // Graph<float, 16> graph(display, 0, 0, 30, 15);
-
-void setup() {
-  pointSpacing = SCREEN_WIDTH / POINT_COUNT;
-
-  display.begin();
-  display.setPowerSave(0);
-  display.setFont(u8g2_font_helvR08_tf);
-
-  Serial.begin(9600);
-
-  display.drawFrame(0, SCREEN_HEIGHT - GRAPH_HEIGHT, SCREEN_WIDTH, GRAPH_HEIGHT);
-  display.setCursor(0, 10);
-  display.print("Temp: ?F");
-
-  display.sendBuffer();
-}
 
 template<typename T, int N>
 T getMax(CircularBuffer<T, N> buffer) {
@@ -81,7 +64,22 @@ void drawPoint(int i, float min, float max) {
   display.drawDisc(xCurr, yCurr, GRAPH_MARGIN - 1);
 }
 
-void graphData() {
+String getData() {
+  String rawValue = Serial.readString();
+  rawValue.trim();
+  return rawValue;
+}
+
+void updateText(String rawValue) {
+  display.setCursor(0, 10);
+  display.print("Temp: ");
+  display.print(rawValue.c_str());
+  display.print("F");
+}
+
+void updateGraph(String rawValue) {
+  dataPoints.append(rawValue.toFloat());
+
   display.drawFrame(0, SCREEN_HEIGHT - GRAPH_HEIGHT, SCREEN_WIDTH, GRAPH_HEIGHT);
 
   float min = getMin(dataPoints);
@@ -96,19 +94,29 @@ void graphData() {
   }
 }
 
+void setup() {
+  pointSpacing = SCREEN_WIDTH / POINT_COUNT;
+
+  display.begin();
+  display.setPowerSave(0);
+  display.setFont(u8g2_font_helvR08_tf);
+
+  Serial.begin(9600);
+
+  display.drawFrame(0, SCREEN_HEIGHT - GRAPH_HEIGHT, SCREEN_WIDTH, GRAPH_HEIGHT);
+  display.setCursor(0, 10);
+  display.print("Temp: ?F");
+
+  display.sendBuffer();
+}
+
 void loop() {
   if (!Serial.available()) return;
 
+  String rawValue = getData();
+
   display.clearBuffer();
-
-  String rawValue = Serial.readString();
-  rawValue.trim();
-  dataPoints.append(rawValue.toFloat());
-  graphData();
-  display.setCursor(0, 10);
-  display.print("Temp: ");
-  display.print(rawValue.c_str());
-  display.print("F");
-
+  updateText(rawValue);
+  updateGraph(rawValue);
   display.sendBuffer();
 }
